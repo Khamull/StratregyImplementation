d1e83db9d73c68d304afefc23e7ef6df70593e71
Charles Path <charles.path@ebankit.com>
2023-09-11 23:38:19 +0100

    first commit

---
+++ SquareFighter/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IMoveStrategy
+{
+    void ExecuteMove(ref int x, ref int y, ConsoleKeyInfo keyInfo);
+}
+
+public class MoveUp : IMoveStrategy
+{
+    public void ExecuteMove(ref int x, ref int y, ConsoleKeyInfo keyInfo)
+    {
+        y = Math.Max(y - 1, 0);
+    }
+}
+
+public class MoveLeft : IMoveStrategy
+{
+    public void ExecuteMove(ref int x, ref int y, ConsoleKeyInfo keyInfo)
+    {
+        x = Math.Max(x - 1, 0);
+    }
+}
+
+public class MoveDown : IMoveStrategy
+{
+    public void ExecuteMove(ref int x, ref int y, ConsoleKeyInfo keyInfo)
+    {
+        y = Math.Min(y + 1, Console.WindowHeight - 1);
+    }
+}
+
+public class MoveRight : IMoveStrategy
+{
+    public void ExecuteMove(ref int x, ref int y, ConsoleKeyInfo keyInfo)
+    {
+        x = Math.Min(x + 1, Console.WindowWidth - 1);
+    }
+}
+
+public interface IDrawStrategy
+{
+    void Draw(int x, int y);
+}
+
+public class StickFigureDrawStrategy : IDrawStrategy
+{
+    public void Draw(int x, int y)
+    {
+        Console.SetCursorPosition(x, y);
+        Console.WriteLine(" O ");
+        Console.SetCursorPosition(x, y + 1);
+        Console.WriteLine("/|\\");
+        Console.SetCursorPosition(x, y + 2);
+        Console.WriteLine("/ \\");
+    }
+}
+
+public class Character
+{
+    private IMoveStrategy moveStrategy;
+    private IDrawStrategy drawStrategy;
+
+    public Character(IMoveStrategy moveStrategy, IDrawStrategy drawStrategy)
+    {
+        this.moveStrategy = moveStrategy;
+        this.drawStrategy = drawStrategy;
+    }
+
+    public void SetMoveStrategy(IMoveStrategy strategy)
+    {
+        this.moveStrategy = strategy;
+    }
+
+    public void SetDrawStrategy(IDrawStrategy strategy)
+    {
+        this.drawStrategy = strategy;
+    }
+
+    public void PerformMove(ref int x, ref int y, ConsoleKeyInfo keyInfo)
+    {
+        this.moveStrategy.ExecuteMove(ref x, ref y, keyInfo);
+    }
+
+    public void Draw(int x, int y)
+    {
+        this.drawStrategy.Draw(x, y);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int x = 0;
+        int y = 0;
+
+        Character fighter = new Character(new MoveUp(), new StickFigureDrawStrategy());
+
+        Dictionary<ConsoleKey, IMoveStrategy> moveStrategies = new Dictionary<ConsoleKey, IMoveStrategy>
+        {
+            { ConsoleKey.W, new MoveUp() },
+            { ConsoleKey.A, new MoveLeft() },
+            { ConsoleKey.S, new MoveDown() },
+            { ConsoleKey.D, new MoveRight() }
+        };
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Press 'W' to move up, 'A' to move left, 'S' to move down," +
+                          " 'D' to move right, 'Q' to Quit.");
+            fighter.Draw(x, y+1);
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+            if (keyInfo.Key == ConsoleKey.Q)
+            {
+                return;
+            }
+
+            if (moveStrategies.ContainsKey(keyInfo.Key))
+            {
+                moveStrategies[keyInfo.Key].ExecuteMove(ref x, ref y, keyInfo);
+            }
+        }
+    }
+}
---
+++ StrategyPattern/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+#region Interface
+// Define an interface for the scheduling strategy
+public interface ISchedulingStrategy
+{
+    void Execute();
+}
+#endregion
+#region Scheduling Logics
+// Implement concrete scheduling strategies
+public class FCFS : ISchedulingStrategy
+{
+    public void Execute()
+    {
+        Console.WriteLine("Executing First-Come-First-Served scheduling strategy.");
+        // Implement FCFS logic here
+    }
+}
+
+public class SJN : ISchedulingStrategy
+{
+    public void Execute()
+    {
+        Console.WriteLine("Executing Shortest-Job-Next scheduling strategy.");
+        // Implement SJN logic here
+    }
+}
+
+public class PriorityScheduling : ISchedulingStrategy
+{
+    public void Execute()
+    {
+        Console.WriteLine("Executing Priority Scheduling strategy.");
+        // Implement Priority Scheduling logic here
+    }
+}
+
+public class RoundRobin : ISchedulingStrategy
+{
+    public void Execute()
+    {
+        Console.WriteLine("Executing Round-Robin scheduling strategy.");
+        // Implement Round-Robin logic here
+    }
+}
+
+public class SRTN : ISchedulingStrategy
+{
+    public void Execute()
+    {
+        Console.WriteLine("Executing Shortest Remaining Time Next (SRTN) scheduling strategy.");
+        // Implement SRTN logic here
+    }
+}
+#endregion
+#region Strategy Class
+// Context class that uses the strategy
+public class Scheduler
+{
+    private ISchedulingStrategy? _strategy;
+
+    public void SetStrategy(ISchedulingStrategy strategy)
+    {
+        _strategy = strategy;
+    }
+
+    public void ExecuteStrategy()
+    {
+        _strategy.Execute();
+    }
+}
+#endregion
+class Program
+{
+    static void Main()
+    {
+        Scheduler scheduler = new Scheduler();
+
+        while (true)
+        {
+            Console.WriteLine("Choose a scheduling strategy:");
+            Console.WriteLine("1. First-Come-First-Served");
+            Console.WriteLine("2. Shortest-Job-Next");
+            Console.WriteLine("3. Priority Scheduling");
+            Console.WriteLine("4. Round-Robin");
+            Console.WriteLine("5. Shortest Remaining Time Next (SRTN)");
+            Console.WriteLine("6. Exit");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            if (choice == 6)
+            {
+                break; // Exit the loop and terminate the program
+            }
+
+            ISchedulingStrategy selectedStrategy = null;
+
+            switch (choice)
+            {
+                case 1:
+                    selectedStrategy = new FCFS();
+                    break;
+                case 2:
+                    selectedStrategy = new SJN();
+                    break;
+                case 3:
+                    selectedStrategy = new PriorityScheduling();
+                    break;
+                case 4:
+                    selectedStrategy = new RoundRobin();
+                    break;
+                case 5:
+                    selectedStrategy = new SRTN();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Using First-Come-First-Served by default.");
+                    selectedStrategy = new FCFS();
+                    break;
+            }
+
+            scheduler.SetStrategy(selectedStrategy);
+            Console.Clear(); // Clear console before execution
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Executing the selected strategy...");
+            Console.ResetColor();
+
+            // Simulate some time-consuming task
+            System.Threading.Thread.Sleep(2000); // Sleep for 2 seconds
+
+            scheduler.ExecuteStrategy();
+
+            Console.WriteLine("\nPress Enter to continue...");
+            Console.ReadLine();
+            Console.Clear(); // Clear console for the next iteration
+        }
+    }
+}
